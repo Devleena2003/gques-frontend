5dfe3217781f025323551499bc5a08db2ed6beb9
Devleena Das <devleena2003@gmail.com>
2022-10-25 12:12:48 +0530

    Added changes

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+<Router>
+<Navbar/>
+<Routes>
+  <Route path='/'exact component={Home} />
+</Routes>
+</Router>
+    
+    </>
+  );
+}
+
+export default App;
